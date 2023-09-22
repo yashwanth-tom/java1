0408820be4e4125d0162eb489e8e5d5f9dafa365
SuviSiva <rsuvisivadme2000@gmail.com>
2023-09-22 07:44:36 +0530

    For Loop

---
+++ app1/src/app1/Leap.java
@@ -0,0 +1,85 @@
+
+package app1;
+
+
+
+public class Leap {
+    public static void main(String[] args)
+    {
+      //looping -for loop 
+        /*
+        for loop -> Range 1,10
+        for(initial;condition;increment or degrement)
+        {
+           statement
+        }
+        */
+        
+//        for(int i=1;i<=10;i++)
+//        {
+//        System.out.println("RamKumar");        
+//        }
+        
+//        for(int i=1;i<=1000;i++)
+//        {
+//            System.out.println("Mala "+i);
+//        }
+//        for(int i=25;i<=100;i+=5)
+//        {
+//            System.out.println(i);
+//        }
+        
+//        for(int i=10;i>0;i--)
+//        {
+//            System.out.println(i);
+//        }
+        
+//        for(int i=1;i<=100;i+=2)
+//        {
+//            System.out.println(i);
+//        }
+//         for(int i=2;i<=100;i+=2)
+//        {
+//            System.out.println(i);
+//        }
+//         for(int i=65;i<=90;i+=2)
+//        {
+//            System.out.print((char)i+" : "+i+",");
+//        }
+        
+//        
+//        System.out.println("Char\tNumber");
+//        System.out.println("----\t------  |");
+//        for(int i=65;i<=90;i++)
+//        {
+//            System.out.println((char)i+"\t"+i+"\t|");
+//        }
+//        System.out.println("----\t------  |");
+    }
+}
+/*
+Scanner input = new Scanner(System.in);
+        System.out.print("Enter any Year : ");
+        int y=input.nextInt();
+        
+        if(y%4==0)
+        {
+            if(y%100==0)
+            {
+                System.out.println("Century year");
+                if(y%400==0)
+                {
+                    System.out.println("Leap year");
+                }
+                else{
+                    System.out.println("Not Leap Year");
+                }
+            }
+            else{
+                System.out.println("Leap Year");
+            }
+        }
+        else{
+            System.out.println("Not Leap Year");
+        }
+*/
